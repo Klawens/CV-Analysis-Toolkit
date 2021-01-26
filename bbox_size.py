97cb3025232cb176476a402d333309cd248f8238
Shichang Liu <klawens@memtrail.com>
2021-01-26 21:21:46 +0800

    Create bbox_size.py

---
+++ bbox_size.py
@@ -0,0 +1,25 @@
+import json
+import matplotlib.pyplot as plt
+
+plt.rcParams['figure.figsize'] = (8.0, 8.0)
+
+j = open('instances_train2017.json', 'r')
+f = json.load(j)
+size = []
+n = 0
+m = 0
+min_scales = 4
+min_strides = 4
+max_scales = 20
+max_strides = 64
+thr_min = (min_scales * min_strides)**2
+thr_max = (max_scales * max_strides)**2
+for i in f['annotations']:
+    size.append(i['bbox'][2] * i['bbox'][3])
+    if i['bbox'][2] * i['bbox'][3] >= thr_max:
+        m += 1
+    if i['bbox'][2] * i['bbox'][3] <= thr_min:
+        n += 1
+print('Min anchor size: {}\nMax anchor size: {}\n{} anchors smaller than threshold {} x {}\n{} anchors bigger than threshold {} x {}'.format(min(size), max(size), n, min_scales, min_strides, m, max_scales, max_strides))
+# plt.plot(size)
+# plt.show()
