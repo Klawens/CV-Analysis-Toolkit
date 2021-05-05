e9197d0bf2a954b32605c6513f00ad40566915a6
Shichang Liu <klawens@memtrail.com>
2021-05-05 21:50:07 +0800

    Update visualizing_val.py

--- coco/visualizing_val.py
+++ coco/visualizing_val.py
@@ -2,8 +2,8 @@ import cv2
 import os
 import json
 
-img_path = '/home/lsc/Kaggle/xray_gt/gt/'
-with open('/home/lsc/Kaggle/result.json', 'r') as f:
+img_path = 'p_v/'
+with open('xxx.bbox.json', 'r') as f:
     json_f = json.load(f)
 img_list = os.listdir(img_path)
 
@@ -11,7 +11,7 @@ for fname in img_list:
     img = cv2.imread(img_path + fname)
     print(img_path + fname)
     for ann in json_f:
-        if ann['name'] == fname:
+        if ann['image_id'] + '.jpg' == fname and ann['score'] > 0.2:
             x1, y1 = ann['bbox'][0], ann['bbox'][1]
             x2, y2 = ann['bbox'][2], ann['bbox'][3]
             # Draw bboxes
@@ -19,4 +19,4 @@ for fname in img_list:
             # Add texts
             # text = str(ann['category'])
             # cv2.putText(img, text, (int(x1), int(y1 - 1)), 2, 2, (0, 0, 255), 1)
-    cv2.imwrite('/home/lsc/Kaggle/xray_gt/inference/%s.jpg' % ann['name'], img)
+            cv2.imwrite('p_r/%s.jpg' % ann['name'], img)
