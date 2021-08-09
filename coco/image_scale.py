a1f317b8f9b7b24d7890d76117b7583da9ca176d
Shichang Liu <klawens@memtrail.com>
2021-08-09 23:38:49 +0800

    Update image_scale.py

--- coco/image_scale.py
+++ coco/image_scale.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 from PIL import Image
 from matplotlib import pyplot as plt
+from tqdm import tqdm
 
 plt.figure(figsize=(5, 5))
 plt.grid(True)
@@ -9,19 +10,22 @@ plt.xlabel('width')
 plt.ylabel('height')
 plt.title('scale analysis')
 
-IMG_PATH = './train2017/'
+IMG_PATH = '../trainData/Images/'
 IMG_LIST = os.listdir(IMG_PATH)
 shape = []
 w = []
 h = []
 
-for img in IMG_LIST:
+n = 0
+for img in tqdm(IMG_LIST):
     img = Image.open(IMG_PATH + img)
     w.append(img.size[0])
     h.append(img.size[1])
-
+    if (img.size[0] > 2000) or (img.size[1] > 2000):
+        n += 1
+print(n)
 shape = np.array([w, h]).transpose()
-print(shape)
+# print(shape)
 plt.plot(w, h, 'o')
 plt.show()
 # plt.savefig('./fig.jpg', format='jpg')
